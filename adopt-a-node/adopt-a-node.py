3009ac4406f575022f9dbe846965703e79f089c1
Sina Habibian <sinahab@gmail.com>
2017-06-14 10:54:50 -0700

    Setup basic Flask server.

--- adopt-a-node/adopt-a-node.py
+++ adopt-a-node/adopt-a-node.py
@@ -1,10 +1,28 @@
 
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
 app = Flask(__name__)
 
-@app.route("/") # take note of this decorator syntax, it's a common pattern
-def hello():
-    return "Hello World!"
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
 
 if __name__ == "__main__":
-    app.run()
+    app.secret_key = os.urandom(12)
+    app.run(debug=True ,host='0.0.0.0', port=3000)
