6572222812c6a9682f4346a2fbe9f69a895f161b
Sina Habibian <sinahab@gmail.com>
2017-06-30 12:26:42 -0700

    Add mobile slider nav.

--- app/utils/assets.py
+++ app/utils/assets.py
@@ -3,17 +3,22 @@ from flask_assets import Bundle
 bundles = {
     'js': Bundle(
         'js/vendor/jquery-3.2.1.min.js',
+        'js/vendor/iscroll.min.js',
+        'js/vendor/drawer.min.js',
         'js/adopt.js',
+        'js/main.js',
         output='dist/main.js'),
 
     'css': Bundle(
         'css/vendor/normalize.css',
         'css/vendor/basscss.css',
         'css/vendor/_font-family.css',
+        'css/vendor/drawer.css',
         'css/colors.styl',
         'css/utils.styl',
         'css/fonts.styl',
         'css/forms.styl',
+        'css/header.styl',
         'css/main.styl',
         'css/node.styl',
         'css/landing.styl',
