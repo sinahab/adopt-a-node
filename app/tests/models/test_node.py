a5d2e7eac6b8d429ceb741b8531b7b13a2724707
Sina Habibian <sinahab@gmail.com>
2017-07-04 19:18:53 -0700

    Write better tests for Node model

--- app/tests/models/test_node.py
+++ app/tests/models/test_node.py
@@ -1,12 +1,11 @@
 
+import datetime
 from app.tests.test_base import TestBase
 from unittest.mock import patch
 
 from app import db
 
 import sqlalchemy
-
-from flask import current_app
 
 from app.models.node import Node
 from app.service_objects.aws_node_manager import AWSNodeManager
@@ -43,9 +42,36 @@ class TestNode(TestBase):
         node_two = Node(provider='digital_ocean', name="Bob's node")
         self.assertEqual(node_two.node_manager().__class__, DigitalOceanNodeManager)
 
-    def test_configure(self):
+    @patch('app.models.node.configure_node')
+    @patch('app.models.node.DigitalOceanNodeManager')
+    @patch('app.models.node.AWSNodeManager')
+    def test_provision(self, mock_aws_node_manager, mock_digital_ocean_node_manager, mock_configure_task):
+        """
+        Test that:
+        1) The correct node manager is used to provision an instance.
+        2) The launched_at value for the node is updated.
+        3) A celery task to configure the node is queued for 45 minutes from now.
+        """
+        node = Node(provider='digital_ocean', name="Bob's node")
+        node_manager = mock_digital_ocean_node_manager.return_value
+
+        self.assertEqual(node.launched_at, None)
+
+        node.provision()
+
+        node_manager.create_server_from_latest_snapshot.assert_called()
+        self.assertTrue(datetime.datetime.now(datetime.timezone.utc) - node.launched_at < datetime.timedelta(minutes=1))
+        mock_configure_task.apply_async.assert_called_with(args=(node.id,), countdown=2700)
+
+    @patch('app.models.node.DigitalOceanNodeManager')
+    @patch('app.models.node.AWSNodeManager')
+    def test_configure(self, aws_node_manager, digital_ocean_node_manager):
         """
         Test that message is relayed to the appropriate node manager.
         """
-        print(current_app.config['BITPAY_TOKEN'])
-        return
+        node = Node(provider='aws', name="Bob's node", status='provisioned')
+        aws_manager_instance = aws_node_manager.return_value
+        node.configure()
+        aws_node_manager.assert_called()
+        aws_manager_instance.update_bitcoin_conf.assert_called()
+        digital_ocean_node_manager.assert_not_called()
