88a34fba0a79272455c1277df0828e8761187a0b
Sina Habibian <sinahab@gmail.com>
2017-08-02 17:59:32 -0700

    Add admin nodes page

--- app/admin/views.py
+++ app/admin/views.py
@@ -4,6 +4,12 @@ from flask_security import current_user, login_required, roles_required
 
 from . import admin
 
+from app.models.user import User
+from app.models.node import Node
+from app.serializers.admin_node_serializer import AdminNodeSerializer
+
+NODES_PER_PAGE = 10
+
 @admin.route('/admin/', methods=['GET'])
 @roles_required('admin')
 @login_required
@@ -11,7 +17,14 @@ def index():
     """
     Show the admin dashboard
     """
-    return render_template('admin/index.html', title="Admin")
+    node_count = len(Node.query.filter_by(status='up').all())
+    user_count = len(User.query.all())
+    counts = {
+        'user_count': user_count,
+        'node_count': node_count
+    }
+
+    return render_template('admin/index.html', title="Admin", counts=counts)
 
 @admin.route('/admin/users/', methods=['GET'])
 @roles_required('admin')
@@ -20,4 +33,14 @@ def users():
     """
     Show the admin users dashboard
     """
-    return render_template('admin/users.html', title="Admin")
+    return render_template('admin/users.html', title="Admin -> Users")
+
+@admin.route('/admin/nodes/<int:page>', methods=['GET'])
+@roles_required('admin')
+@login_required
+def nodes(page=1):
+    """
+    Show the admin nodes dashboard
+    """
+    nodes = Node.query.paginate(page, NODES_PER_PAGE, False)
+    return render_template('admin/nodes.html', nodes=nodes, title="Admin -> Nodes")
